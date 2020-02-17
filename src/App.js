67fd0d24d3d5fa486e6cc0d680e3a13a0be52671
deftorty <bjahmad4tech@gmail.com>
2020-02-17 12:20:18 +0100

    Create boilerplate react app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Header from './Components/Home/Header/Header'
+
+import './App.scss'
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+
+
+export default App;
---
+++ src/Components/Home/Header/Header.js
@@ -0,0 +1,71 @@
+import React from 'react'
+
+import { Container, Row, Col, Button, Navbar, Nav } from 'react-bootstrap'
+
+
+import './Header.scss'
+
+const navbarStyles = {
+    margin: 0,
+    padding: 0
+};
+
+const innerWrapperStyles = {
+    position: "relative",
+};
+
+const mottoStyles = {
+    color: "rgb(0, 54, 0)",
+    width: "400px",
+    height: "auto",
+    marginRight: "3em",
+    marginTop: "3em",
+    fontSize: "1.5rem"
+};
+
+const buttonStyles = {
+    color: "rgb(0, 54, 0)", 
+    background: "white", 
+    border: "1px solid white",
+    borderRadius: "10px",
+    paddingLeft: "1em", 
+    marginRight: "3em", 
+    marginTop: "2em",
+    width: "40%"
+};
+
+const Header = () => {
+    return (
+        <Container className="wrapper" fluid>
+            <Navbar style={navbarStyles} fixed='top'>
+                <Container style={innerWrapperStyles}>
+                    <Navbar.Brand href="/">WEKA</Navbar.Brand>
+
+                    <Navbar.Collapse id="basic-nav-toggle">
+                        <Nav style={{position: "absolute", right: "0", paddingRight: "10px"}}>
+                            <Nav.Link href="#about">About Us</Nav.Link>
+                            <Nav.Link href="#review">Review</Nav.Link>
+                            <Nav.Link href="/login">Login</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+            <Row>
+                <Col sm={12} className="text-right" style={mottoStyles}>
+                    <h1>
+                        Invest. <br />
+                        Profit. <br />
+                        Invest Again. <br />
+                    </h1>
+                </Col>
+
+                <Col sm={{span: "3", offset: "9"}}>
+                    <Button style={buttonStyles}>Sign In</Button>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Header
